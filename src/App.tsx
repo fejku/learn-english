07b02d5d21440c77c7013292588515902e90daee
fejku <przecinek45@o2.pl>
2023-08-04 22:22:03 +0200

    working mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Header from "./components/Header";
 
-import "./App.css";
-
 import icon1 from "./assets/img/animal/ant.svg";
 import icon2 from "./assets/img/animal/bat.svg";
 import icon3 from "./assets/img/animal/bear.svg";
@@ -11,15 +9,21 @@ const App = () => {
     <div className="bg-cyan-700 w-screen h-screen flex flex-col">
       <Header />
       <main className="flex-1 flex flex-col w-full">
-        <button className="bg-red-200 image h-1/3">
-          <img src={icon1} alt="Image 1" width={50} height={50} />
-        </button>
-        <button className=" bg-red-400 image h-1/3">
-          <img src={icon2} alt="Image 2" width={50} height={50} />
-        </button>
-        <button className=" bg-red-600 image h-1/3">
-          <img src={icon3} alt="Image 3" width={50} height={50} />
-        </button>
+        <div className="h-1/3 p-4 flex justify-center">
+          <button className="bg-cyan-600 h-full aspect-square border border-black rounded-lg p-2 hover:bg-cyan-500">
+            <img src={icon1} alt="Image 1" draggable={false} />
+          </button>
+        </div>
+        <div className="h-1/3 p-4 flex justify-center">
+          <button className="bg-cyan-600 h-full aspect-square border border-black rounded-lg p-2 hover:bg-cyan-500">
+            <img src={icon2} alt="Image 2" draggable={false} />
+          </button>
+        </div>
+        <div className="h-1/3 p-4 flex justify-center">
+          <button className="bg-cyan-600 h-full aspect-square border border-black rounded-lg p-2 hover:bg-cyan-500">
+            <img src={icon3} alt="Image 3" draggable={false} />
+          </button>
+        </div>
       </main>
     </div>
   );

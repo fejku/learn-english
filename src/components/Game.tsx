c4c81eeed0d3482cd8dbe1eff18fc9d550fa2a6f
fejku <przecinek45@o2.pl>
2023-08-06 11:00:39 +0200

    moved images into public folder

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,23 +1,23 @@
-import icon1 from "../assets/img/animal/butterfly.svg";
-import icon2 from "../assets/img/animal/bat.svg";
-import icon3 from "../assets/img/animal/bear.svg";
-
 const Game = () => {
   return (
     <main className="flex-1 flex flex-col w-full items-center sm:flex-row">
       <div className="h-1/3 p-4 flex justify-center sm:w-1/3 sm:h-auto sm:block">
         <button className="bg-cyan-600 h-full aspect-square border border-black rounded-lg p-2 hover:bg-cyan-500 sm:h-auto sm:w-full">
-          <img src={icon1} alt="Image 1" draggable={false} />
+          <img
+            src={`imgs/animal/butterfly.svg`}
+            alt="Image 1"
+            draggable={false}
+          />
         </button>
       </div>
       <div className="h-1/3 p-4 flex justify-center sm:w-1/3 sm:h-auto sm:block">
         <button className="bg-cyan-600 h-full aspect-square border border-black rounded-lg p-2 hover:bg-cyan-500 sm:h-auto sm:w-full">
-          <img src={icon2} alt="Image 2" draggable={false} />
+          <img src={`imgs/animal/bat.svg`} alt="Image 2" draggable={false} />
         </button>
       </div>
       <div className="h-1/3 p-4 flex justify-center sm:w-1/3 sm:h-auto sm:block">
         <button className="bg-cyan-600 h-full aspect-square border border-black rounded-lg p-2 hover:bg-cyan-500 sm:h-auto sm:w-full">
-          <img src={icon3} alt="Image 3" draggable={false} />
+          <img src={`imgs/animal/bear.svg`} alt="Image 3" draggable={false} />
         </button>
       </div>
     </main>

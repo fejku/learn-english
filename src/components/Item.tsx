78ef2b326d6b0203e79b10badb0d576957bce3d2
fejku <przecinek45@o2.pl>
2023-08-08 00:23:09 +0200

    changed images, changed css for images to by square

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,6 +3,8 @@ import { IItem } from "../interfaces/IItem";
 import CorrectIcon from "../assets/correct.svg";
 import WrongIcon from "../assets/wrong.svg";
 
+import classes from "./Item.module.css";
+
 type Props = {
   item: IItem;
   index: number;
@@ -28,20 +30,21 @@ const Item: React.FC<Props> = ({
       className="h-1/3 p-4 flex justify-center sm:w-1/3 sm:h-auto sm:block"
     >
       <button
-        className="relative bg-cyan-600 h-full aspect-square border border-black rounded-lg p-2 hover:bg-cyan-500 sm:h-auto sm:w-full"
+        className={`relative bg-cyan-600 h-full aspect-square border border-black rounded-lg hover:bg-cyan-500 sm:h-auto sm:w-full ${classes.item_btn}`}
         onClick={onClick}
       >
         <img
           src={`imgs/animal/${item.filename}.svg`}
           alt={`Image ${index}`}
           draggable={false}
+          className="absolute top-0 left-0 p-2 w-full aspect-square"
         />
         {isPicked && (
           <img
             src={isCorrect ? CorrectIcon : WrongIcon}
             alt={isCorrect ? "Correct icon" : "Wrong icon"}
             draggable={false}
-            className="absolute top-0 left-0 aspect-square p-2"
+            className="absolute top-0 left-0 p-2 w-full aspect-square"
           />
         )}
       </button>
